7ebf5b21b0bf37d1f1b44c80e47bbcb52c7ef05a
maino <mainohgz@gmail.com>
2021-12-16 10:44:27 -0300

    Changed Add User form icons

--- views/dashboard-add-user.php
+++ views/dashboard-add-user.php
@@ -6,11 +6,11 @@
         new Field("text", "first-name", "Nombre/s", fAIcon:"fas fa-user", label:"Nombre"),
         new Field("text", "last-name", "Apellido/s", fAIcon:"far fa-user", label:"Apellido"),
         new Field("password", "pwd", "Contraseña", Validation::$PWD, "fas fa-key", "Contraseña"),
-        new Field("text", "id", "DN/LC/Pasaporte", fAIcon:"fas fa-envelope", label:"Numero de documento"),
-        new Field("date", "born-date", "", fAIcon: "fas fa-phone", label:"Fecha de nacimiento"),
-        new Field("email", "email", "Correo electronico", fAIcon:"fas fa-map-marker-alt", label:"Email"),
-        new Field("text", "category", "Categoria", fAIcon:"fas fa-map-marked-alt", label:"Categoria"),
-        new Field("date", "entry-date", "", fAIcon:"fas fa-map-marked-alt", label:"Fecha de ingreso"),
+        new Field("text", "id", "DN/LC/Pasaporte", fAIcon:"fas fa-id-card", label:"Numero de documento"),
+        new Field("date", "born-date", "", fAIcon: "fas fa-birthday-cake", label:"Fecha de nacimiento"),
+        new Field("email", "email", "Correo electronico", fAIcon:"fas fa-envelope", label:"Email"),
+        new Field("text", "category", "Categoria", fAIcon:"fas fa-trophy", label:"Categoria"),
+        new Field("date", "entry-date", "", fAIcon:"fas fa-calendar-day", label:"Fecha de ingreso"),
     ], "Guardar", $params["fieldValues"] ?? []);
     ?>
     
