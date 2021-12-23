c51b3022bdf12b219d582b5d95a13ae680a1ee70
maino <mainohgz@gmail.com>
2021-12-23 09:00:55 -0300

    Edited jquery script

--- views/layouts/jsScripts.php
+++ views/layouts/jsScripts.php
@@ -1,4 +1,4 @@
 <!-- jQuery -->
-<script src="<?php echo App::getBaseUrl() ?>/plugins/jquery/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo App::getBaseUrl() ?>/dist/js/adminlte.min.js"></script>
